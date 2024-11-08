c2eb2132e706d0debdf4b8077bf7b410a298c8b9
rivershen <caesarshen@gmail.com>
2024-11-08 16:05:54 +0000

    checkpoint

--- test/empty.cpp
+++ test/empty.cpp
@@ -16,12 +16,40 @@ struct C {
 struct D {
   D(B &b, C &c) {}
 };
-template <typename EntryPoint> struct MySystem : public Bluepoint<EntryPoint> {
+
+template<typename T>
+struct TYPE {
+};
+
+template <typename T> struct MySystem  {
+  using EntryPoint = T;
+  DagFactory<EntryPoint> *m_factory = nullptr;
+   template<typename R, typename ... Args> R& create(Args &&... args) {
+    return m_factory->template dedicated<R>(std::forward<Args>(args)...);
+  }
+
+   template<typename R, typename ... Args> R& create2(TYPE<R> t,Args &&... args) {
+    return m_factory->template dedicated<R>(std::forward<Args>(args)...);
+  }
+  template <typename R>
+  void test(const R* xxx){}
   //using Bluepoint<EntryPoint>::create;
-  A &a() { return this->template create<A>(); }
-  B &b() { return m_factory->shared<B>(a()); }
+  A &a() { test("aaa"); return create<A>(); }
+  A& a2(){return create2(TYPE<A>());}
+ /* B &b() { return m_factory->template shared<B>(a()); }
   C &c() { return m_factory->dedicated<C>(a(), b()); }
-  D &d() { return m_factory->dedicated<D>(b(), c()); }
+  D &d() { return m_factory->dedicated<D>(b(), c()); }*/
+};
+
+template <typename T> struct MySystem2: public MySystem<T>  {
+  
+  using MySystem<T>::create;
+  using MySystem<T>::create2;
+  B &b(){return this-> template create<B>();}
+  B& b1(DagFactory<T>* factory) {
+     this->test("aaa");
+     return create2(TYPE<B>());
+  }
 };
 
 template <typename T> struct Base {
